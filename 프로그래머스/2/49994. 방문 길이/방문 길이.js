0d4bb81d2c99a12c6b1a604389c8a3e667f022c4
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2024-11-26 17:48:44 +0900

    [level 2] Title: 방문 길이, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,33 @@
+function solution(dirs) {
+    let x = 5;
+    let y = 5;
+    const set = new Set()
+    const arr = []
+    for(let i of dirs){
+        if(i === "U"){
+            if(y < 10) {
+                y += 1
+                set.add(`${x} ${y} ${x} ${y - 1}`)
+            }
+        }
+        if(i === "D"){
+            if(y > 0) {
+                y -= 1
+                set.add(`${x} ${y + 1} ${x} ${y}`)
+            }
+        }
+        if(i === "R"){
+            if(x < 10) {
+                x += 1
+                set.add(`${x} ${y} ${x - 1} ${y}`)
+            }
+        }
+        if(i === "L"){
+            if(x > 0) {
+                x -= 1
+                set.add(`${x + 1} ${y} ${x} ${y}`)
+            }
+        }
+    }
+    return [...set].length
+}
