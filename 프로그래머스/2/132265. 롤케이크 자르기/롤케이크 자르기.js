08e9c8a01f0a5e302f399dcbd6a996bd755ce62e
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2024-11-28 01:01:34 +0900

    [level 2] Title: 롤케이크 자르기, Time: 134.48 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,20 @@
+function solution(topping) {
+    let count = 0;
+    const st = new Map();
+    const nd = new Map();
+    st.set(topping[0], 1)
+    for(let i = 1; i < topping.length; i++){
+        nd.get(topping[i]) ? nd.set(topping[i], nd.get(topping[i]) + 1) : nd.set(topping[i], 1)
+    }
+    for(let i = 1; i < topping.length - 1; i++){
+        st.get(topping[i]) ? st.set(topping[i], st.get(topping[i]) + 1) : st.set(topping[i], 1)
+        if(nd.get(topping[i])){
+            nd.set(topping[i], nd.get(topping[i]) - 1)
+            if(nd.get(topping[i]) === 0) nd.delete(topping[i])
+        } else {
+            nd.set(topping[i], 1)
+        }
+        if(st.size === nd.size) count += 1
+    }
+    return count
+}
