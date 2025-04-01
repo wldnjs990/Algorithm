b98b14b789bee7eede7048114948fe3a7b53f3ea
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-04-01 10:12:32 +0900

    [level 2] Title: H-Index, Time: 0.68 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,22 +1,7 @@
 function solution(citations) {
-    // H-index : 자신 이상으로 인용된 논문의 숫자 >= 자신 && 자신 >= 자신 보다 낮게 인용된 논문의 숫자
-    // 그런 논문 중에서 가장 큰 수를 리턴해야함
-    const result = []
-    const max = Math.max(...citations)
-    
-    for(let i = 0; i <= max; i++){
-        const h = i
-        let low = 0
-        let height = 0
-        citations.forEach(now=>{
-            if(now >= h) {
-                height += 1
-            } else {
-                low += 1
-            }
-        })
-        if(h <= height && h >= low) result.push(h)
-    }
-    
-    return Math.max(...result)
+    let i = 0;
+    const sorted = citations.sort((a, b) => b - a)
+    while(i + 1 <= sorted[i]) i++;
+
+    return i;
 }
