cde5afc7d85bb0cb5c9270c49a8296f9dd00bfc4
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-06-02 19:49:16 +0900

    [level 1] Title: 최소직사각형, Time: 5.55 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,19 +1,12 @@
 // 2차원 배열 순회하면서 1차원 배열의 크기를 비교해서 거기서 나오는 큰 숫자는 큰 숫자들이랑 비교, 작은 숫자들은 작은 숫자들이랑 비교하면서 값 도출하면 되지 않을까
 function solution(sizes) {
-    let long = 0;
-    let short = 0;
-    sizes.forEach((e)=>{
-        let eLong;
-        let eShort;
-        if(e[0] > e[1]){
-            eLong = e[0]
-            eShort = e[1]
-        } else {
-            eLong = e[1]
-            eShort = e[0]
-        }
-        if(eLong > long) long = eLong;
-        if(eShort > short) short = eShort;
+    const a = []
+    const b = []
+    sizes.forEach(e=>{
+        if(e[0] < e[1]) e.reverse()
+        a.push(e[0])
+        b.push(e[1])
     })
-    return long * short
+    console.log(a, b)
+    return Math.max(...a) * Math.max(...b)
 }
