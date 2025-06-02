aebde4ec50a6d215daad072b7e2f4d50afc1f628
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-06-02 15:52:07 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 439.17 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -2,18 +2,19 @@
 // 
 
 function solution(bridge_length, weight, truck_weights) {
+    const queue = []
+    const move = []
     let count = 1
-    const queue = [[truck_weights.shift(), bridge_length]]
-
-    while(truck_weights.length || queue.length){
-        // 1초 추가하고, 현재 도로에 있는 차량들 거리 -1
+    while(truck_weights.length){
+        if(queue.reduce((acc,cur)=>acc+cur,0) + truck_weights[0] <= weight){
+            queue.push(truck_weights.shift())
+            move.push(bridge_length)
+        }
+        move.forEach((_, idx)=>{
+            move[idx] -= 1
+            if(move[idx] === 0) queue.shift()
+        })
         count += 1
-        for(let i = 0; i < queue.length; i++) queue[i][1] -= 1
-        if(queue[0][1] === 0) queue.shift()
-        // 현재 도로에 있는 차들의 무게를 측정하고, 다음 차량을 더한 값 보다 weight가 낮으면 차량 queue 추가
-        const sum = queue.reduce((acc, cur)=>{return acc + cur[0]}, 0)
-        if(sum + truck_weights[0] <= weight) queue.push([truck_weights.shift(), bridge_length])
-        // 현재 첫 번째 차량이 목적지에 도달했으면 차량 제거
     }
-    return count
+    return count + move[move.length - 1]
 }
