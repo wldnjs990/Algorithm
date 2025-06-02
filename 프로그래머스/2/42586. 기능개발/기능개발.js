5cd27f5217fb426b928d7a7d34d625cccf9b1062
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-06-02 11:39:47 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,27 +1,16 @@
 function solution(progresses, speeds) {
     const result = []
     while(progresses.length){
-        let count = 0
-        // 첫 번째 기능이 완성되는 날짜 구하기
-        const frontEnded = Math.ceil((100 - progresses[0]) / speeds[0])
-        // 모든 배열을 첫 번째 기능이 완성되는 날짜 만큼 속도 더해주기
-        progresses = progresses.map((e, idx)=>{
-            return e += (speeds[idx] * frontEnded)
-        })
-        // 첫 번째 기능부터 100을 넘겼으면 현재 배열에서 제거 + 결과에 추가해주고 그렇지 않으면 반복문 중단
-        const progressesLen = progresses.length
-        for(let i = 0; i < progressesLen; i++){
-            if(progresses[0] >= 100){
-                progresses.shift()
-                speeds.shift()
-                count += 1
-            } else {
-                break
-            }
+        for(let i = 0; i < progresses.length; i++){
+            progresses[i] += speeds[i]
         }
-        
-        // 무조건 한 개 이상은 카운트가 쌓였을테니 result에 값 추가해주기
-        result.push(count)
+        let count = 0
+        while(progresses[0] >= 100){
+            progresses.shift()
+            speeds.shift()
+            count += 1
+        }
+        if(count > 0) result.push(count)
     }
     return result
 }
