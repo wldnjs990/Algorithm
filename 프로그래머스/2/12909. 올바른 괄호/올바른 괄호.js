3a93960b41b81048bfc1522b12480d3ff85a6938
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-06-02 11:51:01 +0900

    [level 2] Title: 올바른 괄호, Time: 32.44 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,16 @@
 function solution(s){
-    const result = []
-    for(let e of s){
-        if(e === "("){
-            result.push(e)
+    const queue = []
+    if(s[0] === ')' || s[s.length - 1] === '(') return false
+    for(let i = 0; i < s.length; i++){
+        if(!queue.length){
+            if(s[i] === ')') return false
+            else queue.push(s[i])
+        } else if(s[i] === queue[queue.length - 1]){
+            queue.push(s[i])
         } else {
-            if(result.length !== 0) result.pop(0)
-            else return false
+            queue.pop()
         }
     }
-    return result.length === 0 ? true : false
+    if(queue.length) return false
+    else return true
 }
