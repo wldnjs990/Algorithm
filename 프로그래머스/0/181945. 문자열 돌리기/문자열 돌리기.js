6891719bc3c1632b6adb437700c5e5b5604eccce
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-06-30 10:46:49 +0900

    [level 0] Title: 문자열 돌리기, Time: 43.53 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    let str = input[0];
+    str.split('').forEach(e=>{
+        console.log(e)
+    })
+});
