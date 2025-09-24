0bb4b0a55410c639c1a0b2d262b25c136edd2951
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-09-24 21:29:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 40992 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,59 +1,55 @@
 # 숨바꼭질
 
-# 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고
-# 동생은 점 K(0 ≤ K ≤ 100,000)에 있다.
-# 수빈이는 걷거나 순간이동을 할 수 있다
-# 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
+# 수빈이는 동생과 숨바꼭질을 하고 있다. 
+# 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 
+# 동생은 점 K(0 ≤ K ≤ 100,000)에 있다. 
+
+# 수빈이는 걷거나 순간이동을 할 수 있다. 
+# 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 
 # 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
 
-# 수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
+# 수빈이와 동생의 위치가 주어졌을 때, 
+# 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
 
-N, K = map(int, input().split())
+# [입력]
+# 첫 번째 줄에 수빈이가 있는 위치 N과 동생이 있는 위치 K가 주어진다. N과 K는 정수이다.
 
-# 현재위치 - 1
-# 현재위치 + 1
-# 현재위치 * 2
-# 이 3가지 캐이스들 전부 반복하면서 동생 위치를 넘어가지 않는 선에서 도착 카운트 구하기
-# 이건 최단거리 구하기라서 BFS 맞네
+# [출력]
+# 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
 
-# 방문여부
-visited = [False for _ in range(K)]
+# [문제 풀이]
+# BFS 활용하는 문제
+# visited 배열 만들고 시작점 부터 -1, +1, *2 케이스를 전부 실행해서 가장 먼저 동생한테 도착했을때 return 해주면 됨
+# 최대 거리가 100_000까지인데, 널널하게 200_000으로 잡자
 
-def BFS(start):
-    global time
-    queue = [start]
-    while queue:
-        now, ctn = queue.pop(0)
+from collections import deque
 
-        # 거리가 0보다 작아지면 종료
-        if now < 0:
-            continue
-        # 동생보다 더 앞서갔으면 나머지 거리 계산 후 time 비교해서 time 업데이트 하고 종료
-        if now > K:
-            if time > ctn + (now - K):
-                time = ctn + (now - K)
-            continue
-        # 동생한테 도착하면 종료
-        if now == K:
-            if time > ctn:
-                time = ctn
-            continue
+def BFS():
+    deq = deque()
+    # 시작 위치 초기화(내 위치, 0초)
+    deq.append([N, 0])
+    while deq:
+        now, time = deq.popleft()
+        # 동생한테 도착했으면 걸린 시간 반환
+        if now == M:
+            return time
+        
+        # 경로 체크 후 모든 이동경로 돌기
+        if now + 1 < 200_000 and not visited[now + 1]:
+            visited[now + 1] = True
+            deq.append([now+1, time+1])
+        if now - 1 < 200_000 and now - 1 >= 0 and not visited[now - 1]:
+            visited[now - 1] = True
+            deq.append([now-1, time+1])
+        if now * 2 < 200_000 and not visited[now*2]:
+            visited[now*2] = True
+            deq.append([now*2, time+1])
 
-        # 방문한 지역이면 이동 중단
-        if visited[now - 1]:
-            continue
-        # 방문 체크
-        visited[now - 1] = True
+# 나 = N, 동생 = M
+N, M = map(int, input().split())
+# 최대 10만인데, 널널하게 20만으로 설정
+visited = [False] * 200_001
 
-        # 뒤로
-        queue.append([now - 1, ctn + 1])
-        # 앞으로
-        queue.append([now + 1, ctn + 1])
-        # 순간이동
-        queue.append([now * 2, ctn + 1])
+ans = BFS()
 
-# 최단시간 구하기
-time = 100_000_000
-BFS([N, 0])
-
-print(time)
+print(ans)
